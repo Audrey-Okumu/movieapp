74a084443a757611681db8869f4bf635be8f09d5
Audrey Okumu <okumuaudrey71@gmail.com>
2026-07-02 20:29:10 +0300

    add: added movie entity, repository and service

---
+++ src/main/java/com/audrey/movieapp/movie/MovieService.java
@@ -0,0 +1,13 @@
+package com.audrey.movieapp.movie;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+}
