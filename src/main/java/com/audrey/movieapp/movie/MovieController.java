e059eddcaf7b653fb8aee8aab0fff5a0e57eb92f
Audrey Okumu <okumuaudrey71@gmail.com>
2026-07-14 10:51:13 +0300

    "feat: Aded get by id controller"

---
+++ src/main/java/com/audrey/movieapp/movie/MovieController.java
@@ -0,0 +1,22 @@
+package com.audrey.movieapp.movie;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/{movieId}")
+    public Movie getMovie(@PathVariable Long movieId) {
+        return movieService.getMovieById(movieId);
+    }
+}
